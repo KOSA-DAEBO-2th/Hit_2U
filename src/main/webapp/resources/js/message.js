ddb134b31e4318abff4a383af82a56afb4e34967
ShinJA <jiae980627@naver.com>
2023-09-14 09:08:18 +0900

    [추가]쪽지 전송, 리스트 로드

--- src/main/webapp/resources/js/message.js
+++ src/main/webapp/resources/js/message.js
@@ -10,17 +10,90 @@ function m_write(){
 	$('.main_area').load("message_write");
 }
 
+function m_sendList(){
+	$('.main_area').load("message_slist");
+}
+
+/*
+function selectAll(selectAll){
+	const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+	checkboxes.forEach((checkbox)=> {
+		checkbox.checked = selectAll.checked
+	})
+}
+*/
+
+$(document).ready(function() {
+	$("#cbx_chkAll").click(function() {
+		console.log("clicked");
+		if($("#cbx_chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
+		else $("input[name=chk]").prop("checked", false);
+	});
+
+	$("input[name=chk]").click(function() {
+		var total = $("input[name=chk]").length;
+		var checked = $("input[name=chk]:checked").length;
+
+		if(total != checked) $("#cbx_chkAll").prop("checked", false);
+		else $("#cbx_chkAll").prop("checked", true); 
+	});
+});
+
+
 
 function m_send(){
-	var to = document.getElementById('to').value;
-	console.log(to);
-	var m_content = document.getElementById('message').value;
+	console.log("clickedddddd");
+	var m_receive = document.getElementById('m_receive').value;
+	console.log(m_receive);
+	var m_content = document.getElementById('m_content').value;
 	console.log(m_content);
+	var message = { "m_receive": m_receive, "m_content": m_content}
+	console.log(message);
 
 	$.ajax({
 		type: "POST",
-		url: "/message_send",
-		data: {'to':to, 'm_content':m_content},
-		success: send_message(data)
+		url: "message_send",
+		contentType: 'application/json',
+		data: JSON.stringify(message),
+		success: function(data){
+			console.log("success");
+			//location.href = "message_list";
+			m_list();
+
+		}
 	});  
 }
+
+
+function del() {
+	//alert("삭제하시겠습니까?");
+	var confirm_val = confirm("삭제하시겠습니까?");
+	
+	if(confirm_val) {
+		var checkArr = new Array();
+		$("input[class='chk']:checked").each(function(){
+			checkArr.push($(this).val());
+		});
+		
+		$.ajax({
+		type: "POST",
+		url: "message_del",
+		contentType: 'application/json',
+		data: JSON.stringify(message),
+		success: function(data){
+			console.log("success");
+			//location.href = "message_list";
+			m_list();
+
+		}
+	});
+	}
+	
+	let list = [];
+	$(".chk:checked").each(function(idx, item){
+	if(idx == 0){
+		list.push({"m_no":item});
+	} else {
+		list += ", "
+}
+
