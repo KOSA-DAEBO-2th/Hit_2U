95ec58ed0d3023b0def94d19b2b2f0fa4827df72
Yangseungyun <supersatori@naver.com>
2023-09-14 09:47:30 +0900

    [수정] meeting.js 오류수정

--- src/main/webapp/resources/js/meeting.js
+++ src/main/webapp/resources/js/meeting.js
@@ -9,6 +9,3 @@ $(".btn_anonymous").on("click", function () {
     $(".btn_category").removeClass("btn_category_click");
     $(this).addClass("btn_anonymous_click");
 });
-
-var data = ${list[0].tags}
-
